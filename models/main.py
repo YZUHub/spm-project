4866cfb781c963370e418a0b94f4a087b0d2b3b2
rifatrakib <Rakib.1508@outlook.com>
2024-12-11 19:38:47 +0800

    modify: perform etl on data to reduce size and implement working model training logic

--- models/main.py
+++ models/main.py
@@ -1,20 +1,31 @@
 from pathlib import Path
 
-from loader import prepare_data
-from regressors import CatBoost, LGB, XGB
+from sklearn.model_selection import GridSearchCV
+
+from regressors import CatBoost, LightGBM, XGBoost
 
 
 def train():
-    sources = Path("data").rglob("*.jsonl")
-    for source in sources:
-        X, y = prepare_data(source)
-        print(f"Training model on {source} with {len(X)} records")
-        break
-
     cat = CatBoost()
-    print(cat.feature_preprocessors())
-    print(cat.prepare_pipeline())
-    print(cat.prepare_grid_search())
+
+    X_train, X_test, y_train, y_test = cat.train_test_split()
+
+    cat_gs: GridSearchCV = cat.grid_search(X_train, y_train)
+    print(f"{cat_gs.best_params_ = }")
+    y_pred = cat_gs.predict(X_test)
+    print(f"{cat.evaluate_model(y_test, y_pred) = }")
+
+    lgbm = LightGBM()
+    lgbm_gs = lgbm.grid_search(X_train, y_train)
+    print(f"{lgbm_gs.best_params_ = }")
+    y_pred = lgbm_gs.predict(X_test)
+    print(f"{lgbm.evaluate_model(y_test, y_pred) = }")
+
+    xgb = XGBoost()
+    xgb_gs = xgb.grid_search(X_train, y_train)
+    print(f"{xgb_gs.best_params_ = }")
+    y_pred = xgb_gs.predict(X_test)
+    print(f"{xgb.evaluate_model(y_test, y_pred) = }")
 
 
 if __name__ == "__main__":
--- models/regressors.py
+++ models/regressors.py
@@ -1,92 +1,79 @@
 from abc import ABC, abstractmethod
-from pathlib import Path
+from typing import Iterator
 
-import jsonlines
+import numpy as np
 import pandas as pd
 from catboost import CatBoostRegressor
 from lightgbm import LGBMRegressor
-from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import BaseCrossValidator, GridSearchCV
 from xgboost import XGBRegressor
+
+
+class TimeseriesSplitter(BaseCrossValidator):
+    def __init__(self, n_splits: int = 3):
+        self.validation_folds = [[2014, 2016], [2017, 2019], [2020, 2022]]
+        self.n_splits = n_splits
+
+    def split(self, X: pd.DataFrame, y: pd.Series, groups: None = None) -> Iterator[tuple[np.ndarray, np.ndarray]]:
+        for validation_years in self.validation_folds:
+            train_indices = X[X["year"] < validation_years[0]].index
+            val_indices = X[X["year"].isin(validation_years)].index
+            yield train_indices, val_indices
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        """Returns the number of splitting iterations"""
+        return self.n_splits
 
 
 class Regressor(ABC):
     def __init__(self):
         self.regressor = None
-        self.categorical_features = ['unit_type', 'building_type_name_aggregated', 'energy_label', 'ownership_form']
-        self.features = [
-            "postal_code",
-            "unit_type",
-            "building_type_name_aggregated",
-            "floor_number",
-            "floors",
-            "land_area",
-            "bta",
-            "bra",
-            "prom",
-            "rooms",
-            "bedrooms",
-            "bathrooms",
-            "built_year",
-            "energy_label",
-            "bar",
-            "wcs",
-            "building_enr",
-            "ownership_form",
-            "nearest_train_station_distance",
-            "nearest_bus_station_distance",
-            "nearest_ferry_terminal_distance",
-            "nearest_tram_station_distance",
-            "nearest_underground_station_distance",
-            "nearest_gondola_lift_station_distance",
-            "nearest_airport_distance",
-            "nearest_kindergartens_distance",
-            "nearest_elementary_middle_school_distance",
-            "nearest_high_school_distance",
-            "nearest_fire_station_distance",
-            "in_beach_zone",
-            "date",
-            "lat",
-            "lon",
-            "year",
-            "month",
-        ]
+        self.test_years = [2023, 2024]
+        self.cv_splitter = TimeseriesSplitter()
+        self.best_model = None
+        self.cv_results = None
 
     @abstractmethod
     def get_grid_params(self) -> dict:
         return {}
 
-    def load_data(self, filename: str) -> tuple[pd.DataFrame, pd.Series]:
-        with jsonlines.open(filename) as reader:
-            df = pd.DataFrame(reader)
+    def load_data(self) -> tuple[pd.DataFrame, pd.Series]:
+        df = pd.read_csv("data/timeseries-sample.csv")
+        df = df.drop(columns=[df.columns[0]])
+        print(f"Successfully loaded data with shape {df.shape = }")
+        df["in_beach_zone"] = df["in_beach_zone"].astype(int)
 
-        df["lat"] = df["geometry"].apply(lambda x: x["coordinates"][1])
-        df["lon"] = df["geometry"].apply(lambda x: x["coordinates"][0])
-        df["date"] = pd.to_datetime(df["date"])
-        df["year"] = df["date"].dt.year
-        df["month"] = df["date"].dt.month
+        X, y = df.drop(columns=["index"]), df["index"]
+        return X, y
 
-        return df[self.features], df["index"]
+    def train_test_split(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+        X, y = self.load_data()
+        X_train, X_test = X[X["year"].isin(self.test_years) == False], X[X["year"].isin(self.test_years)]
+        y_train, y_test = y[X["year"].isin(self.test_years) == False], y[X["year"].isin(self.test_years)]
+        print(f"Successfully split data into train and test sets with shapes {X_train.shape = }, {X_test.shape = }")
+        return X_train, X_test, y_train, y_test
 
-    def feature_preprocessors(self) -> ColumnTransformer:
-        # Create preprocessor (only for categorical features)
-        encoder = OneHotEncoder(drop="first", sparse_output=False, handle_unknown="ignore")
-        preprocessor = ColumnTransformer(transformers=[("cat", encoder, self.categorical_features)], remainder="passthrough")
-        return preprocessor
+    def evaluate_model(self, y_true: pd.Series, y_pred: np.ndarray) -> dict:
+        return {
+            'MSE': mean_squared_error(y_true, y_pred),
+            'RMSE': np.sqrt(mean_squared_error(y_true, y_pred)),
+            'MAE': mean_absolute_error(y_true, y_pred),
+            'R2': r2_score(y_true, y_pred)
+        }
 
-    def prepare_pipeline(self) -> Pipeline:
-        return Pipeline([
-            ("preprocessor", self.feature_preprocessors()),
-            ("regressor", self.regressor),
-        ])
+    def grid_search(self, X: pd.DataFrame, y: np.ndarray) -> GridSearchCV:
+        gs = GridSearchCV(
+            self.regressor,
+            self.get_grid_params(),
+            scoring="neg_mean_squared_error",
+            cv=self.cv_splitter,
+            n_jobs=-1,
+            verbose=3,
+        )
 
-    def prepare_grid_search(self):
-        pipeline = self.prepare_pipeline()
-        params = self.get_grid_params()
-        grid_search = GridSearchCV(pipeline, params, cv=3, n_jobs=-1, verbose=2)
-        return grid_search
+        gs.fit(X, y)
+        return gs
 
 
 class CatBoost(Regressor):
@@ -96,42 +83,36 @@ class CatBoost(Regressor):
 
     def get_grid_params(self) -> dict:
         return {
-            'regressor__n_estimators': [100, 200, 300],
-            'regressor__learning_rate': [0.01, 0.1, 0.3],
-            'regressor__max_depth': [3, 4, 5],
-            'regressor__l2_leaf_reg': [1, 3, 5],
-            'regressor__subsample': [0.8, 0.9, 1.0],
-            'regressor__colsample_bylevel': [0.8, 0.9, 1.0]
+            'iterations': [100, 200],
+            'learning_rate': [0.01, 0.05],
+            'depth': [4, 6],
+            # 'l2_leaf_reg': [3, 5],
         }
 
 
-class LGB(Regressor):
+class LightGBM(Regressor):
     def __init__(self):
         super().__init__()
-        self.regressor = LGBMRegressor(random_state=42)
+        self.regressor = LGBMRegressor(random_state=42, verbose=-1)
 
     def get_grid_params(self) -> dict:
         return {
-            'regressor__n_estimators': [100, 200, 300],
-            'regressor__learning_rate': [0.01, 0.1, 0.3],
-            'regressor__max_depth': [3, 4, 5],
-            'regressor__num_leaves': [7, 15, 31],
-            'regressor__subsample': [0.8, 0.9, 1.0],
-            'regressor__colsample_bytree': [0.8, 0.9, 1.0]
+            'n_estimators': [100, 200],
+            'learning_rate': [0.01, 0.05],
+            'max_depth': [4, 6],
+            # 'num_leaves': [31, 63],
         }
 
 
-class XGB(Regressor):
+class XGBoost(Regressor):
     def __init__(self):
         super().__init__()
         self.regressor = XGBRegressor(tree_method="hist", random_state=42)
 
     def get_grid_params(self) -> dict:
         return {
-            'regressor__n_estimators': [100, 200, 300],
-            'regressor__learning_rate': [0.01, 0.1, 0.3],
-            'regressor__max_depth': [3, 4, 5],
-            'regressor__min_child_weight': [1, 3, 5],
-            'regressor__subsample': [0.8, 0.9, 1.0],
-            'regressor__colsample_bytree': [0.8, 0.9, 1.0]
+            'n_estimators': [100, 200],
+            'learning_rate': [0.01, 0.05],
+            'max_depth': [4, 6],
+            # 'min_child_weight': [1, 3, 5],
         }
