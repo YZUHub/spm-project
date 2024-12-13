398f28ddfd29453c888516c26b14ef3ea031a2dd
rifatrakib <Rakib.1508@outlook.com>
2024-12-14 00:39:35 +0800

    add: client application added with header containing navbar

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcomeFallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
