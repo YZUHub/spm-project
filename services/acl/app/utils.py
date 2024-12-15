4db02e8feed5c33b310f0a7fa6c2e1be9d15c9bd
rifatrakib <Rakib.1508@outlook.com>
2024-12-15 17:51:31 +0800

    update: OTP module supports better security

--- services/acl/app/utils.py
+++ services/acl/app/utils.py
@@ -2,5 +2,6 @@ from datetime import datetime, timezone
 
 
 def validate_otp(otp: int) -> bool:
-    date = datetime.now(timezone.utc).strftime("%y-%m-%d").replace("-", "")
-    return str(otp) == date
+    timestamp = datetime.now(timezone.utc)
+    valid_otp = f"{str(timestamp.day).zfill(2)}{str(timestamp.hour).zfill(2)}{str(timestamp.minute).zfill(2)}"
+    return str(otp) == valid_otp
